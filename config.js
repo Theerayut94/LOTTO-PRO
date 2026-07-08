1c2eb7c106f9aed2645b10fde182e8a37abe30a5
Theerayut94 <62866340+Theerayut94@users.noreply.github.com>
2026-07-08 11:59:04 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -8,7 +8,7 @@
 // https://script.google.com/macros/s/xxxxxxxxxxxxxxxx/exec
 
 
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyOv3skB0mNE8TZ5sa9sVbEb5TctkpzE6bioJ7ylgiU9uqRud-Jdw2g7v68iCy6hyCL/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyh-AkdXoHFODHypwdcYxwWndY3wTD7_2f3S3MJd1Ue90x_YuhEyqWTs8_fcHypp4eL/exec";
 
 
 
